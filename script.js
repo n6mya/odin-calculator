688b0655b80a0379840c5d2a32cfe0eef5b99b34
n6mya <g4tyaa@gmail.com>
2024-11-26 12:42:29 +0700

    create variables for displaying operation in DOM

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let add = (num_1, num_2) => num_1 + num_2;
 let subtract = (num_1, num_2) => num_1 - num_2;
 let multiply = (num_1, num_2) => num_1 * num_2;
 let divide = (num_1, num_2) => num_1 / num_2;
+
+let number_1 = 0;
+let number_2 = 0;
+let operator = '';

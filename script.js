7c68d34edbf0e560235531ac1f858907678a1070
n6mya <g4tyaa@gmail.com>
2024-11-26 12:40:16 +0700

    define functions for operation

---
+++ script.js
@@ -0,0 +1,4 @@
+let add = (num_1, num_2) => num_1 + num_2;
+let subtract = (num_1, num_2) => num_1 - num_2;
+let multiply = (num_1, num_2) => num_1 * num_2;
+let divide = (num_1, num_2) => num_1 / num_2;

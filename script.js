f70c597ccc1c9e04c4c7f7fb8ae4588ce3f93bc3
n6mya <g4tyaa@gmail.com>
2024-11-28 16:45:53 +0700

    add selectors to buttons and display

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function operate(number_1, number_2, operator){
     if (operator == "*") return multiply(number_1, number_2);
     if (operator == "/") return divide(number_1, number_2);
 }
+
+const numOne = document.querySelector('.1');
+const numTwo = document.querySelector('.2');
+const numThree = document.querySelector('.3');
+const numFour = document.querySelector('.4');
+const numFive = document.querySelector('.5');
+const numSix = document.querySelector('.6');
+const numSeven = document.querySelector('.7');
+const numEight = document.querySelector('.8');
+const numNine = document.querySelector('.9');
+const numZero = document.querySelector('.0');
+
+
+let display = document.querySelector('.numberdisplay');

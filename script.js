b13f17e379df07e745f9071070a003437a9fb0e2
n6mya <g4tyaa@gmail.com>
2024-11-28 16:23:35 +0700

    define function to operate based on input

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ let divide = (num_1, num_2) => num_1 / num_2;
 let number_1 = 0;
 let number_2 = 0;
 let operator = '';
+
+function operate(number_1, number_2, operator){
+    if (operator == "+") return add(number_1, number_2);
+    if (operator == "-") return subtract(number_1, number_2);
+    if (operator == "*") return multiply(number_1, number_2);
+    if (operator == "/") return divide(number_1, number_2);
+}
